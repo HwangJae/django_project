819b1e29da6bc7f0c77e3086924948557d3c26b5
HwangJae <67469524+HwangJae@users.noreply.github.com>
2020-07-15 19:04:07 +0900

    ch3장 틀만들고, 블로그만들기, 꾸며보기, 태그까지(7/15)

--- ch97/blog/views.py
+++ ch97/blog/views.py
@@ -3,14 +3,14 @@ from django.views.generic import ArchiveIndexView, YearArchiveView, MonthArchive
 from django.views.generic import DayArchiveView, TodayArchiveView
 
 from blog.models import Post
-
+from django.conf import settings
 
 #--- ListView
 class PostLV(ListView):
     model = Post
     template_name = 'blog/post_all.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 3
 
 
 #--- DetailView
@@ -57,4 +57,12 @@ class TaggedObjectLV(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tagname'] = self.kwargs['tag']
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['disqus_short'] = f"{settings.DISQUS_SHORTNAME}"
+        context['disqus_id'] = f"post-{self.object.id}-{self.object.slug}"
+        context['disqus_url'] = f"{settings.DISQUS_MY_DOMAIN}{self.object.get_absolute_url()}"
+        context['disqus_title'] = f"{self.object.slug}"
         return context
